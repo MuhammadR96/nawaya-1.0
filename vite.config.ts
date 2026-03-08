a41e13d44a3a47fcd6be59c87926aac15bb6a3ca
MuhammadR96 <83252990+MuhammadR96@users.noreply.github.com>
2026-03-09 00:14:49 +0200

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nawaya-8d925408/',  // Your repository name
+  base: '/nawaya-1.0/',  // Your repository name
 })
